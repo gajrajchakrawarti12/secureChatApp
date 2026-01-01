caf873657d37bc1240463e4a9a090061493fe7df
gajrajchakrawarti12 <gajrajchakrawarti12@gmail.com>
2026-01-01 09:04:47 +0530

    resolve AuthException message format

--- app/lib/src/core/errors/auth_exception.dart
+++ app/lib/src/core/errors/auth_exception.dart
@@ -4,5 +4,5 @@ class AuthException implements Exception {
   AuthException([this.message = 'Authentication error']);
   
   @override
-  String toString() => message;
+  String toString() => 'AuthException: $message';
 }
